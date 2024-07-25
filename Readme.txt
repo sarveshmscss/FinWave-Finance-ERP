451d57746375399a5eebd6e322aa3c373f919e66
sarveshmscss18 <sarveshmscss@gmail.com>
2024-07-25 14:56:50 +0530

    first commit

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,3 @@
-Thanks for downloading this template!
+Thanks for downloading this FinWave!
 
-Template Name: NiceAdmin
-Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
+Author: Sarvesh  K
--- finwave.py
+++ finwave.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
+import mysql.connector 
 from decimal import Decimal
 from datetime import datetime
 import logging
